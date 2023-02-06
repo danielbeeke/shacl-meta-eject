373215abd573127c5839b78eeeeeff8f5ccf64e7
Daniel Beeke <mail@danielbeeke.nl>
2023-02-06 12:55:35 +0100

    bump

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { ShaclModel } from 'https://deno.land/x/shacl_meta_sparql@0.3/mod.ts'
+export { ShaclModel } from 'https://deno.land/x/shacl_meta_sparql@0.4/mod.ts'
 export { Converter } from 'https://deno.land/x/shacl_meta_type@0.3/mod.ts'
